1dfca0788fe91a051b192466d0d248267f14995d
Maxwell Dubin <maxdhs@gmail.com>
2018-10-19 10:25:06 -0700

    Add jQuery variables for user input options and branch functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
   $('#inputButton').click(function(event) {
     event.preventDefault();
+    var math = $("#math").val();
     var companySize = $("#companySize").val();
     var styleOrFunctionality = $("#styleOrFunctionality").val();
     var webpagesOrApps = $("#webpagesOrApps").val();
-    if (age == "") {
-      alert("Please enter your age");
-    }
-    if (bondActor === "Craig Daniels") {
-      $("h1").hide();
+
+    if (math == "I love math" && companySize == "Large corporation" && styleOrFunctionality == "functionality") {
+      $("h4").hide();
       $('#output1').show();
     }
-    else if (fastFood === "Pizza" && bondActor === "Sean Connery") {
-      $("h1").hide();
+    else if (math == "I avoid math" && companySize == "Small company" && styleOrFunctionality == "Style") {
+      $("h4").hide();
       $('#output2').show();
     }
-    else if (age < 18) {
-      $("h1").hide();
+    else if (math == "I love math" || companySize == "Large corporation") {
+      $("h4").hide();
       $('#output3').show();
     }
+    else if (styleOrFunctionality == "Style") {
+      $("h4").hide();
+      $('#output2').show();
+    }
     else {
-      $('#output3').show();
+      $("h4").hide();
+      $('#output2').show();
     }
   });
 });
